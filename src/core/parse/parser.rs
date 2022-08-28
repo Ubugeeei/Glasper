22d65860af14df250409297f699fd55e9929d70c
Ubugeeei <ubuge1122@gamil.com>
2022-08-28 22:59:27 +0900

    feat: next token

--- src/core/parse/parser.rs
+++ src/core/parse/parser.rs
@@ -1,22 +1,27 @@
 // TODO: remove this
 #![allow(dead_code)]
 
-use crate::core::tokenize::{
-    lexer::Lexer,
-    token::{Token, TokenType},
-};
+use crate::core::tokenize::{lexer::Lexer, token::Token};
 
-pub struct Parser {
-    l: Lexer,
-    cur_tok: Token,
-    peek_tok: Option<Token>,
+pub struct Parser<'a> {
+    l: &'a mut Lexer,
+    cur_token: Token,
+    peeked_token: Token,
 }
-impl Parser {
-    fn new(l: Lexer) -> Self {
+impl<'a> Parser<'a> {
+    fn new(l: &'a mut Lexer) -> Self {
+        let first_token = l.next_token();
+        let secound_token = l.next_token();
+
         Parser {
             l,
-            cur_tok: Token::new(TokenType::InitialValue, "".to_string()),
-            peek_tok: None,
+            cur_token: first_token,
+            peeked_token: secound_token,
         }
+    }
+
+    fn next_token(&mut self) {
+        self.cur_token = self.peeked_token.clone();
+        self.peeked_token = self.l.next_token();
     }
 }
