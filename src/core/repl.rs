6a16c08ce68cad13f022a95b14f0c09fa5b52fcc
Ubugeeei <ubuge1122@gamil.com>
2022-08-28 19:16:50 +0900

    tweak: interpreter message

--- src/core/repl.rs
+++ src/core/repl.rs
@@ -4,26 +4,27 @@ use std::io::{self, Write};
 const PROMPT: &str = "> ";
 
 pub fn start() {
-    loop {
-        print!("{}", PROMPT);
-        io::stdout().flush().unwrap();
+  loop {
+    println!("Welcome to Glasper v0.1.0 ");
+    print!("{}", PROMPT);
+    io::stdout().flush().unwrap();
 
-        let mut input = String::new();
+    let mut input = String::new();
 
-        io::stdin().read_line(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
 
-        if &input == "exit()\n" {
-            println!("Bye!");
-            break;
-        }
-
-        let mut l = Lexer::new(input);
-        let mut t = l.next_token();
-        println!();
-        while t.token_type != TokenType::Eof {
-            println!("{:?}", t);
-            t = l.next_token();
-        }
-        println!();
+    if &input == "exit()\n" {
+      println!("Bye.");
+      break;
     }
+
+    let mut l = Lexer::new(input);
+    let mut t = l.next_token();
+    println!();
+    while t.token_type != TokenType::Eof {
+      println!("{:?}", t);
+      t = l.next_token();
+    }
+    println!();
+  }
 }
