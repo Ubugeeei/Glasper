3fad8d9e824b21a133b3d62bb2875fca903369ea
Ubugeeei <ubuge1122@gamil.com>
2022-09-18 20:57:04 +0900

    feat: eval integer

--- src/core/eval/evaluator.rs
+++ src/core/eval/evaluator.rs
@@ -3,12 +3,30 @@
 use std::io::Error;
 
 use crate::core::{
-    object::def::{GUndefined, Object},
-    parse::ast::Program,
+    object::def::{GNumber, GUndefined, Object},
+    parse::ast::{Expression, Program, Statement},
 };
 
 pub fn eval(_program: &Program) -> Result<Object, Error> {
-    Ok(Object::Undefined(GUndefined))
+    let mut result = Object::Undefined(GUndefined);
+    for statement in &_program.statements {
+        result = eval_statement(statement)?;
+    }
+    Ok(result)
+}
+
+pub fn eval_statement(statement: &Statement) -> Result<Object, Error> {
+    match statement {
+        Statement::Expression(expr) => eval_expression(expr),
+        _ => Ok(Object::Undefined(GUndefined)),
+    }
+}
+
+pub fn eval_expression(expr: &Expression) -> Result<Object, Error> {
+    match expr {
+        Expression::Integer(i) => Ok(Object::Number(GNumber { value: *i })),
+        _ => Ok(Object::Undefined(GUndefined)),
+    }
 }
 
 #[cfg(test)]
@@ -17,14 +35,11 @@ mod tests {
     use crate::core::{parse::parser::Parser, tokenize::lexer::Lexer};
 
     #[test]
-    fn test_eval() {
-        let mut l = Lexer::new("let a = 1;".to_string());
+    fn test_eval_int() {
+        let mut l = Lexer::new("1".to_string());
         let mut p = Parser::new(&mut l);
         let program = p.parse_program();
         assert_eq!(program.statements.len(), 1);
-        assert_eq!(
-            format!("{}", eval(&program).unwrap()),
-            "\x1b[30mundefined\x1b[0m"
-        );
+        assert_eq!(format!("{}", eval(&program).unwrap()), "\x1b[33m1\x1b[0m");
     }
 }
