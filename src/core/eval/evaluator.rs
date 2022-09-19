d4bc9cb5713ba2f3b3bd917faeacb43fa2d1dba6
Ubugeeei <ubuge1122@gamil.com>
2022-09-19 11:35:06 +0900

    feat: store env ref in evaluator

--- src/core/eval/evaluator.rs
+++ src/core/eval/evaluator.rs
@@ -3,124 +3,147 @@
 use std::io::Error;
 
 use crate::core::{
+    eval::environment::Environment,
     object::def::{GBoolean, GNull, GNumber, GUndefined, Object},
-    parse::ast::{Expression, Program, Statement},
+    parse::ast::{Expression, LetStatement, Program, Statement},
 };
 
-pub fn eval(_program: &Program) -> Result<Object, Error> {
-    let mut result = Object::Undefined(GUndefined);
-    for statement in &_program.statements {
-        result = eval_statement(statement)?;
-    }
-    Ok(result)
+pub struct Evaluator<'a> {
+    env: &'a mut Environment,
 }
-
-fn eval_statement(statement: &Statement) -> Result<Object, Error> {
-    match statement {
-        Statement::Expression(expr) => eval_expression(expr),
-        _ => Ok(Object::Undefined(GUndefined)),
+impl<'a> Evaluator<'a> {
+    pub fn new(env: &'a mut Environment) -> Self {
+        Evaluator { env }
     }
-}
 
-fn eval_expression(expr: &Expression) -> Result<Object, Error> {
-    match expr {
-        Expression::Number(i) => Ok(Object::Number(GNumber { value: *i })),
-        Expression::Boolean(b) => Ok(Object::Boolean(GBoolean { value: *b })),
-        Expression::Null => Ok(Object::Null(GNull)),
-        Expression::Undefined => Ok(Object::Undefined(GUndefined)),
-
-        Expression::Prefix(expr) => eval_prefix_expression(expr),
-        Expression::Infix(expr) => {
-            let left = eval_expression(&expr.left)?;
-            let right = eval_expression(&expr.right)?;
-            eval_infix_expression(expr.operator.clone(), left, right)
+    pub fn eval(&self, program: &Program) -> Result<Object, Error> {
+        let mut result = Object::Undefined(GUndefined);
+        for statement in &program.statements {
+            result = self.eval_statement(statement)?;
         }
-
-        _ => Ok(Object::Undefined(GUndefined)),
+        Ok(result)
     }
-}
 
-fn eval_prefix_expression(
-    expr: &crate::core::parse::ast::PrefixExpression,
-) -> Result<Object, Error> {
-    let right = eval_expression(&expr.right)?;
-    match expr.operator.as_str() {
-        "!" => eval_bang_operator_expression(right),
-        "-" => eval_minus_prefix_operator_expression(right),
-        o => Err(Error::new(
-            std::io::ErrorKind::Other,
-            format!(
-                "Unexpected prefix operator '{}'. at eval_prefix_expression",
-                o
-            ),
-        )),
+    fn eval_statement(&self, statement: &Statement) -> Result<Object, Error> {
+        match statement {
+            Statement::Expression(expr) => self.eval_expression(expr),
+            Statement::Let(stmt) => self.eval_let_statement(stmt),
+            _ => Ok(Object::Undefined(GUndefined)),
+        }
     }
-}
 
-fn eval_bang_operator_expression(right: Object) -> Result<Object, Error> {
-    match right {
-        Object::Boolean(GBoolean { value }) => Ok(Object::Boolean(GBoolean { value: !value })),
-        Object::Null(_) => Ok(Object::Boolean(GBoolean { value: true })),
-        Object::Undefined(_) => Ok(Object::Boolean(GBoolean { value: true })),
-        Object::Number(GNumber { value }) => {
-            if value == 0.0 {
-                Ok(Object::Boolean(GBoolean { value: true }))
-            } else {
-                Ok(Object::Boolean(GBoolean { value: false }))
+    fn eval_expression(&self, expr: &Expression) -> Result<Object, Error> {
+        match expr {
+            Expression::Number(i) => Ok(Object::Number(GNumber { value: *i })),
+            Expression::Boolean(b) => Ok(Object::Boolean(GBoolean { value: *b })),
+            Expression::Null => Ok(Object::Null(GNull)),
+            Expression::Undefined => Ok(Object::Undefined(GUndefined)),
+
+            Expression::Prefix(expr) => self.eval_prefix_expression(expr),
+            Expression::Infix(expr) => {
+                let left = self.eval_expression(&expr.left)?;
+                let right = self.eval_expression(&expr.right)?;
+                self.eval_infix_expression(expr.operator.clone(), left, right)
+            }
+
+            _ => Ok(Object::Undefined(GUndefined)),
+        }
+    }
+
+    fn eval_prefix_expression(
+        &self,
+        expr: &crate::core::parse::ast::PrefixExpression,
+    ) -> Result<Object, Error> {
+        let right = self.eval_expression(&expr.right)?;
+        match expr.operator.as_str() {
+            "!" => self.eval_bang_operator_expression(right),
+            "-" => self.eval_minus_prefix_operator_expression(right),
+            o => Err(Error::new(
+                std::io::ErrorKind::Other,
+                format!(
+                    "Unexpected prefix operator '{}'. at eval_prefix_expression",
+                    o
+                ),
+            )),
+        }
+    }
+
+    fn eval_bang_operator_expression(&self, right: Object) -> Result<Object, Error> {
+        match right {
+            Object::Boolean(GBoolean { value }) => Ok(Object::Boolean(GBoolean { value: !value })),
+            Object::Null(_) => Ok(Object::Boolean(GBoolean { value: true })),
+            Object::Undefined(_) => Ok(Object::Boolean(GBoolean { value: true })),
+            Object::Number(GNumber { value }) => {
+                if value == 0.0 {
+                    Ok(Object::Boolean(GBoolean { value: true }))
+                } else {
+                    Ok(Object::Boolean(GBoolean { value: false }))
+                }
             }
         }
     }
-}
 
-fn eval_minus_prefix_operator_expression(right: Object) -> Result<Object, Error> {
-    if let Object::Number(GNumber { value }) = right {
-        Ok(Object::Number(GNumber { value: -value }))
-    } else {
-        Err(Error::new(
-            std::io::ErrorKind::Other,
-            "Unexpected prefix operator. at eval_minus_prefix_operator_expression",
-        ))
+    fn eval_minus_prefix_operator_expression(&self, right: Object) -> Result<Object, Error> {
+        if let Object::Number(GNumber { value }) = right {
+            Ok(Object::Number(GNumber { value: -value }))
+        } else {
+            Err(Error::new(
+                std::io::ErrorKind::Other,
+                "Unexpected prefix operator. at eval_minus_prefix_operator_expression",
+            ))
+        }
     }
-}
 
-fn eval_infix_expression(operator: String, left: Object, right: Object) -> Result<Object, Error> {
-    match (left, right) {
-        (Object::Number(GNumber { value: l }), Object::Number(GNumber { value: r })) => {
-            match operator.as_str() {
-                "+" => Ok(Object::Number(GNumber::new(l + r))),
-                "-" => Ok(Object::Number(GNumber::new(l - r))),
-                "*" => Ok(Object::Number(GNumber::new(l * r))),
-                "/" => Ok(Object::Number(GNumber::new(l / r))),
-                "<" => Ok(Object::Boolean(GBoolean::new(l < r))),
-                ">" => Ok(Object::Boolean(GBoolean::new(l > r))),
-                "==" => Ok(Object::Boolean(GBoolean::new(l == r))),
-                "!=" => Ok(Object::Boolean(GBoolean::new(l != r))),
-                o => Err(Error::new(
-                    std::io::ErrorKind::Other,
-                    format!(
-                        "Unexpected infix operator '{}'. at eval_infix_expression",
-                        o
-                    ),
-                )),
+    fn eval_infix_expression(
+        &self,
+        operator: String,
+        left: Object,
+        right: Object,
+    ) -> Result<Object, Error> {
+        match (left, right) {
+            (Object::Number(GNumber { value: l }), Object::Number(GNumber { value: r })) => {
+                match operator.as_str() {
+                    "+" => Ok(Object::Number(GNumber::new(l + r))),
+                    "-" => Ok(Object::Number(GNumber::new(l - r))),
+                    "*" => Ok(Object::Number(GNumber::new(l * r))),
+                    "/" => Ok(Object::Number(GNumber::new(l / r))),
+                    "<" => Ok(Object::Boolean(GBoolean::new(l < r))),
+                    ">" => Ok(Object::Boolean(GBoolean::new(l > r))),
+                    "==" => Ok(Object::Boolean(GBoolean::new(l == r))),
+                    "!=" => Ok(Object::Boolean(GBoolean::new(l != r))),
+                    o => Err(Error::new(
+                        std::io::ErrorKind::Other,
+                        format!(
+                            "Unexpected infix operator '{}'. at eval_infix_expression",
+                            o
+                        ),
+                    )),
+                }
             }
-        }
-        (Object::Boolean(GBoolean { value: l }), Object::Boolean(GBoolean { value: r })) => {
-            match operator.as_str() {
-                "==" => Ok(Object::Boolean(GBoolean::new(l == r))),
-                "!=" => Ok(Object::Boolean(GBoolean::new(l != r))),
-                o => Err(Error::new(
-                    std::io::ErrorKind::Other,
-                    format!(
-                        "Unexpected infix operator '{}'. at eval_infix_expression",
-                        o
-                    ),
-                )),
+            (Object::Boolean(GBoolean { value: l }), Object::Boolean(GBoolean { value: r })) => {
+                match operator.as_str() {
+                    "==" => Ok(Object::Boolean(GBoolean::new(l == r))),
+                    "!=" => Ok(Object::Boolean(GBoolean::new(l != r))),
+                    o => Err(Error::new(
+                        std::io::ErrorKind::Other,
+                        format!(
+                            "Unexpected infix operator '{}'. at eval_infix_expression",
+                            o
+                        ),
+                    )),
+                }
             }
+            _ => Err(Error::new(
+                std::io::ErrorKind::Other,
+                "Unexpected infix operator. at eval_infix_expression",
+            )),
         }
-        _ => Err(Error::new(
-            std::io::ErrorKind::Other,
-            "Unexpected infix operator. at eval_infix_expression",
-        )),
+    }
+
+    fn eval_let_statement(&self, _stmt: &LetStatement) -> Result<Object, Error> {
+        // TODO: bind value to environment
+
+        Ok(Object::Undefined(GUndefined))
     }
 }
 
@@ -134,9 +157,11 @@ mod tests {
         let mut l = Lexer::new("let a = 1;".to_string());
         let mut p = Parser::new(&mut l);
         let program = p.parse_program();
+        let mut e = Environment::new();
+        let ev = Evaluator::new(&mut e);
         assert_eq!(program.statements.len(), 1);
         assert_eq!(
-            format!("{}", eval(&program).unwrap()),
+            format!("{}", ev.eval(&program).unwrap()),
             "\x1b[30mundefined\x1b[0m"
         );
     }
@@ -146,8 +171,13 @@ mod tests {
         let mut l = Lexer::new("1".to_string());
         let mut p = Parser::new(&mut l);
         let program = p.parse_program();
+        let mut e = Environment::new();
+        let ev = Evaluator::new(&mut e);
         assert_eq!(program.statements.len(), 1);
-        assert_eq!(format!("{}", eval(&program).unwrap()), "\x1b[33m1\x1b[0m");
+        assert_eq!(
+            format!("{}", ev.eval(&program,).unwrap()),
+            "\x1b[33m1\x1b[0m"
+        );
     }
 
     #[test]
@@ -156,9 +186,11 @@ mod tests {
             let mut l = Lexer::new("true".to_string());
             let mut p = Parser::new(&mut l);
             let program = p.parse_program();
+            let mut e = Environment::new();
+            let ev = Evaluator::new(&mut e);
             assert_eq!(program.statements.len(), 1);
             assert_eq!(
-                format!("{}", eval(&program).unwrap()),
+                format!("{}", ev.eval(&program,).unwrap()),
                 "\x1b[33mtrue\x1b[0m"
             );
         }
@@ -166,9 +198,11 @@ mod tests {
             let mut l = Lexer::new("false".to_string());
             let mut p = Parser::new(&mut l);
             let program = p.parse_program();
+            let mut e = Environment::new();
+            let ev = Evaluator::new(&mut e);
             assert_eq!(program.statements.len(), 1);
             assert_eq!(
-                format!("{}", eval(&program).unwrap()),
+                format!("{}", ev.eval(&program,).unwrap()),
                 "\x1b[33mfalse\x1b[0m"
             );
         }
@@ -191,8 +225,10 @@ mod tests {
             let mut l = Lexer::new(input.to_string());
             let mut p = Parser::new(&mut l);
             let program = p.parse_program();
+            let mut e = Environment::new();
+            let ev = Evaluator::new(&mut e);
             assert_eq!(program.statements.len(), 1);
-            assert_eq!(format!("{}", eval(&program).unwrap()), expected);
+            assert_eq!(format!("{}", ev.eval(&program,).unwrap()), expected);
         }
     }
 
@@ -228,8 +264,10 @@ mod tests {
             let mut l = Lexer::new(input.to_string());
             let mut p = Parser::new(&mut l);
             let program = p.parse_program();
+            let mut e = Environment::new();
+            let ev = Evaluator::new(&mut e);
             assert_eq!(program.statements.len(), 1);
-            assert_eq!(format!("{}", eval(&program).unwrap()), expected);
+            assert_eq!(format!("{}", ev.eval(&program,).unwrap()), expected);
         }
     }
 }
