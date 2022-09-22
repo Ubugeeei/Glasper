8b480b6350cc7c1dd2bb5493669dffd7cc803cf3
Ubugeeei <ubuge1122@gamil.com>
2022-09-23 01:32:26 +0900

    feat: help

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,28 @@ mod runtime;
 
 use std::env;
 
+const HELP: &str = r#"
+Usage: gls [options] [ script.js ] [arguments]
+
+Options:
+    -                             script read from stdin (default if no file name is provided, interactive mode if a tty)
+    -v, --version                 print version
+    -h, --help                    print command line options (currently set)
+"#;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         cli::interact::start();
     } else {
-        cli::file::run(&args[1]);
+        match &*args[1] {
+            "-h" | "--help" => {
+                println!("{}", HELP);
+            }
+            "-v" | "--version" => {}
+            arg => {
+                cli::file::run(arg);
+            }
+        }
     }
 }
