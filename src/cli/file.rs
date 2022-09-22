62276d7242f5cb907d60b455d815db338f594b4d
Ubugeeei <ubuge1122@gamil.com>
2022-09-23 01:40:34 +0900

    feat: handle not founded module

--- src/cli/file.rs
+++ src/cli/file.rs
@@ -2,6 +2,17 @@ use crate::runtime::js::JavaScriptRuntime;
 
 pub fn run(path: &str) {
     let mut runtime = JavaScriptRuntime::new();
-    let source = std::fs::read_to_string(path).unwrap();
-    let _ = runtime.execute(source);
+    match std::fs::read_to_string(path) {
+        Ok(source) => {
+            let _ = runtime.execute(source);
+        }
+        Err(_) => {
+            let crr_dir = std::env::current_dir().unwrap();
+            println!(
+                "\x1b[31merror\x1b[0m: Module not found \"file://{}/{}\".",
+                crr_dir.display(),
+                path
+            );
+        }
+    };
 }
