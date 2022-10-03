94988394c660096879c4098e037f22ac54f58e62
Ubugeeei <ubuge1122@gamil.com>
2022-10-03 09:26:14 +0900

    example

--- example/array.js
+++ example/array.js
@@ -2,3 +2,4 @@ const arr = [1, 2, 3, 4, 5];
 console.log("arr.length:", arr.length);
 console.log("arr[0]:", arr[0]);
 console.log("arr[100]:", arr[100]);
+console.log('arr.join("/"):', arr.join("/"));
