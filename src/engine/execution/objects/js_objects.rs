088f4d011f66c580074b06bfcc6c843a937d974b
Ubugeeei <ubuge1122@gmail.com>
2023-05-04 01:38:53 +0900

    feat: object display

--- src/engine/execution/objects/js_objects.rs
+++ src/engine/execution/objects/js_objects.rs
@@ -1,16 +1,41 @@
-use std::{marker::PhantomData, ptr::NonNull};
+#![allow(dead_code)]
+
+use std::{collections::HashMap, fmt::Display, ptr::NonNull};
 
 pub(crate) struct JSObject<T> {
-    #[allow(dead_code)]
     pub(crate) ptr: NonNull<T>,
-    _marker: PhantomData<T>,
+    pub(crate) properties: HashMap<String, JSObject<T>>,
+    _type: JSType,
 }
 
 impl<T> JSObject<T> {
     pub(crate) fn new(ptr: NonNull<T>) -> Self {
         JSObject {
             ptr,
-            _marker: PhantomData,
+            properties: HashMap::new(),
+            _type: JSType::Object,
         }
     }
+}
+
+impl<T> Display for JSObject<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self._type {
+            JSType::Boolean(b) => write!(f, "\x1b[33m{}\x1b[0m", b),
+            JSType::Number(n) => write!(f, "\x1b[33m{}\x1b[0m", n),
+            JSType::String(s) => write!(f, "\x1b[32m'{}'\x1b[0m", s),
+            JSType::Object => write!(f, "\x1b[34m[Object]\x1b[0m"),
+            JSType::Function => write!(f, "[Function]"),
+            JSType::Undefined => write!(f, "\x1b[30mundefined\x1b[0m"),
+        }
+    }
+}
+
+pub(crate) enum JSType {
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Object,
+    Function,
+    Undefined,
 }
